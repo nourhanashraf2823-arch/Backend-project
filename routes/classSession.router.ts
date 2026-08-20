be89d2e2a35385fabd75e35bee9be966b6072a11
RanaTharwat52 <tharwatr160@gmail.com>
2026-08-20 19:28:34 +0300

    Update classSession.router.ts

---
+++ routes/classSession.router.ts
@@ -0,0 +1,41 @@
+ import { Router } from "express";
+
+import {
+  getAllClassSessions,
+  getClassSessionById,
+  createClassSession,
+  updateClassSession,
+  deleteClassSession
+} from "../controllers/classSession.controller";
+
+import { authenticate } from "../middlewares/auth.middleware";
+import { authrizeRoles } from "../middlewares/role.middleware";
+
+const router = Router();
+
+router.get("/", getAllClassSessions);
+
+router.get("/:id", getClassSessionById);
+
+router.post(
+  "/",
+  authenticate,
+  authrizeRoles(["Trainer"]),
+  createClassSession
+);
+
+router.put(
+  "/:id",
+  authenticate,
+  authrizeRoles(["Trainer"]),
+  updateClassSession
+);
+
+router.delete(
+  "/:id",
+  authenticate,
+  authrizeRoles(["Trainer"]),
+  deleteClassSession
+);
+
+export default router;
