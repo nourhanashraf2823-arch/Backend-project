224976fdba491649ab8dcb2087e59cd063364fd0
janahashem14 <janamahmoudhashe@gmail.com>
2026-08-22 10:06:46 +0300

    corrected

--- server.ts
+++ server.ts
@@ -8,7 +8,6 @@ import classSessionRoutes from "./routes/classSession.router";
 import searchRoutes from "./routes/search.router";
 import swaggerUi from "swagger-ui-express";
 import { specs } from "./config/swagger";
-import dns from "dns";
 
 dotenv.config();
 
@@ -16,19 +15,27 @@ const app = express();
 
 app.use(express.json());
 
-
+// Swagger Docs
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
-
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/bookings", bookingRoutes);
 app.use("/api/sessions", classSessionRoutes); 
 app.use("/api/search", searchRoutes);
 
-dns.setServers(["8.8.8.8", "1.1.1.1"]);
-connectDB();
-
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to connect to DB:", error);
+  }
+};
+
+startServer();
